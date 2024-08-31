7e53acc826d3912fc3f5a9ce8d950f7aa02434d5
Tabin Imtiyaz <tabinimtiyazwork85@gmail.com>
2024-08-31 23:02:32 +0530

    Added icons and hover effects on the Gallery page images. Icons and hover effects are responsive, showing only on larger screens.

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { HiOutlineDownload, HiOutlineArrowsExpand } from "react-icons/hi"; // Importing icons from Heroicons
 import BackgroundWrapper from "../components/BackgroundWrapper";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -29,7 +30,7 @@ function Gallery() {
   return (
     <BackgroundWrapper>
       <Navbar />
-      <div className="pt-20 pb-5 sm:mx-5 lg:mx-10 xl:mx-30 min-h-[94vh]">
+      <div className="pt-20 pb-5 sm:mx-10 md:mx-20 lg:mx-20 xl:mx-40 min-h-[94vh]">
         <ResponsiveMasonry
           columnsCountBreakPoints={{
             350: 1,
@@ -37,16 +38,39 @@ function Gallery() {
             900: 3,
           }}
         >
-          <Masonry gutter="30px">
+          <Masonry gutter="20px">
             {images.map((image, index) => (
-              <img
-                key={index}
-                src={image.src}
-                alt={image.alt}
-                onError={(e) => {
-                  e.target.src = placeholder;
-                }} // Fallback to placeholder
-              />
+              <div key={index} className="relative group">
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className="w-full h-auto object-cover"
+                  onError={(e) => {
+                    e.target.src = placeholder;
+                  }}
+                />
+                {/* Combined overlay, logo, and icons container */}
+                <div className="absolute inset-0 md:flex justify-between items-start p-2 bg-black bg-opacity-0 lg:group-hover:bg-opacity-50 transition-opacity duration-300 cursor-zoom-in">
+                  {/* Top-left logo */}
+                  <div className="opacity-0 md:group-hover:opacity-100 transition-opacity duration-300 z-20">
+                    <img
+                      src="/src/assets/Logo.png"
+                      className="w-1/2 h-auto"
+                      alt="Logo"
+                    />
+                  </div>
+
+                  {/* Top-right buttons */}
+                  <div className="flex space-x-2 opacity-0 lg:group-hover:opacity-100 transition-opacity duration-300 z-20">
+                    <button className="text-black bg-white bg-opacity-60 p-2 rounded hover:bg-opacity-80">
+                      <HiOutlineArrowsExpand />
+                    </button>
+                    <button className="text-black bg-white bg-opacity-60 p-2 rounded hover:bg-opacity-80">
+                      <HiOutlineDownload />
+                    </button>
+                  </div>
+                </div>
+              </div>
             ))}
           </Masonry>
         </ResponsiveMasonry>
